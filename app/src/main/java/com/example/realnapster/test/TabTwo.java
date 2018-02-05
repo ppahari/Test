d2f2a79feb4c3d0e66d0d2b00d343b791c352e0d
Paras Pahari <rage2spirit@gmail.com>
2018-02-05 15:35:13 -0500

    Tab Two and its Permission Model Implemented

--- app/src/main/java/com/example/realnapster/test/TabTwo.java
+++ app/src/main/java/com/example/realnapster/test/TabTwo.java
@@ -1,20 +1,131 @@
 package com.example.realnapster.test;
 
+import android.Manifest;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+import static android.support.v4.content.ContextCompat.checkSelfPermission;
 
 /**
  * Created by Realnapster on 2/5/2018.
  */
 
 public class TabTwo extends Fragment {
+    @BindView(R.id.imageView)
+    ImageView imageView;
+    @BindView(R.id.seekBar)
+    SeekBar seekBar;
+    @BindView(R.id.coordinatorLayout)
+    CoordinatorLayout coordinatorLayout;
+
+    Unbinder unbinder;
+    Context mContext;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getActivity();
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_two,container,false);
+        View view = inflater.inflate(R.layout.tab_two, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressValue;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progressValue = i;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                checkCameraPermission();
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void checkCameraPermission() {
+        int hasCameraPermission = checkSelfPermission(mContext, Manifest.permission.CAMERA);
+        if (hasCameraPermission != PackageManager.PERMISSION_GRANTED) {
+            if (!shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, 111);
+                return;
+            }
+            displaySnackBar();
+            return;
+        }
+        displayRandomImage();
+    }
+
+    private void displayRandomImage() {
+        imageView.setImageResource(android.R.drawable.ic_lock_silent_mode);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 111) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                displayRandomImage();
+            }
+        }
+    }
+
+    private void displaySnackBar() {
+        Snackbar snackbar = Snackbar
+                .make(coordinatorLayout, "Please enable camera permisssion", Snackbar.LENGTH_LONG)
+                .setAction("Okay", new View.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.M)
+                    @Override
+                    public void onClick(View view) {
+                        int hasCameraPermission = checkSelfPermission(mContext, Manifest.permission.CAMERA);
+                        if (hasCameraPermission != PackageManager.PERMISSION_GRANTED) {
+                            requestPermissions(new String[]{Manifest.permission.CAMERA}, 111);
+                            return;
+                        }
+                    }
+                });
+
+        snackbar.show();
     }
 }
